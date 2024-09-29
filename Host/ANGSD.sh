04944b0c8dbc6d4263748e706c893a0724930cdf
cbreusing <77281303+cbreusing@users.noreply.github.com>
2024-09-29 11:08:55 -0400

    Add files via upload

--- Host/ANGSD.sh
+++ Host/ANGSD.sh
@@ -17,6 +17,7 @@ module load r/4.2.2-z6qdiis
 module load fastme/2.1.5.1-kmg5til 
 module load gsl/2.7.1-khmyfcy
 module load libnsl/2.0.1-ed2i5hn
+module load angsd/0.935-cbhuwc7
 
 for file in `cat filelist.txt`
 do
@@ -66,49 +67,45 @@ IND=`cat ${POP}.list | wc -l`
 FRAC=`echo "${IND}*0.75" | bc | awk '{print int($1+0.5)}'`
 angsd -P ${SLURM_CPUS_ON_NODE} -bam ${POP}.list -ref A_boucheti.Trinity.merged95.filtered.fasta -gl 1 -doGlf 1 -baq 1 -C 50 -minInd ${FRAC} -minMapQ 30 -minQ 20 -setMinDepth 2 -setMaxDepth 160 -doCounts 1 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -doMaf 1 -doMajorMinor 1 -SNP_pval 1e-6 -dosnpstat 1 -doHWE 1 -sb_pval 0.05 -hetbias_pval 0.05 -makeMatrix 1 -doIBS 1 -doCov 1 -doPost 1 -doGeno 2 -minMaf 0.01 -indF ANGSD_basin-wide/${POP}.indF -skipTriallelic 1 -out ANGSD_basin-wide/A_boucheti
 zcat ANGSD_basin-wide/${POP}.mafs.gz | tail -n+2 | perl -anle 'print $F[0] . "\t" . $F[1]' > ANGSD_basin-wide/sites.txt
-cut -f1 ANGSD_basin-wide/sites.txt | sort | uniq > ANGSD_basin-wide/chrs.txt
-angsd sites index ANGSD_basin-wide/sites.txt
 
 for POP in KM TC TM ABE;
 do
 IND=`cat ANGSD_basin-wide/${POP}.list | wc -l`
 FRAC=`echo "${IND}*0.5" | bc | awk '{print int($1+0.5)}'`
 angsd -P ${SLURM_CPUS_ON_NODE} -bam ANGSD_basin-wide/${POP}.list -ref A_boucheti.Trinity.merged95.filtered.fasta -anc A_boucheti.Trinity.merged95.filtered.fasta -gl 1 -baq 1 -C 50 -minInd ${FRAC} -minMapQ 30 -minQ 20 -setMinDepth 2 -setMaxDepth 160 -doCounts 1 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -doMaf 1 -doMajorMinor 4 -doSaf 2 -indF ANGSD_basin-wide/${POP}.indF -out ANGSD_basin-wide/${POP}
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/${POP}.saf.idx -tole 1e-6 -maxIter 5000 -P ${SLURM_CPUS_ON_NODE} -fold 1 > ANGSD_basin-wide/${POP}.sfs
-angsd -P ${SLURM_CPUS_ON_NODE} -bam ANGSD_basin-wide/${POP}.list -ref A_boucheti.Trinity.merged95.filtered.fasta -anc A_boucheti.Trinity.merged95.filtered.fasta -gl 1 -baq 1 -C 50 -minInd ${FRAC} -sites ANGSD_basin-wide/sites.txt -rf ANGSD_basin-wide/chrs.txt -minMapQ 30 -minQ 20 -setMinDepth 2 -setMaxDepth 160 -doCounts 1 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -doMaf 1 -doMajorMinor 4 -doSaf 2 -indF ANGSD_basin-wide/${POP}.indF -doThetas 1 -pest ANGSD_basin-wide/${POP}.sfs -out ANGSD_basin-wide/${POP}
+realSFS ANGSD_basin-wide/${POP}.saf.idx -tole 1e-6 -maxIter 5000 -P ${SLURM_CPUS_ON_NODE} -fold 1 > ANGSD_basin-wide/${POP}.sfs
 done
 
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TC.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/KM-TC.folded.sfs
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst index ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TC.saf.idx -sfs ANGSD_basin-wide/KM-TC.folded.sfs -fold 1 -fstout ANGSD_basin-wide/KM-TC -whichFST 1 -maxIter 5000
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/ABE.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/KM-ABE.folded.sfs
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst index ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/ABE.saf.idx -sfs ANGSD_basin-wide/KM-ABE.folded.sfs -fold 1 -fstout ANGSD_basin-wide/KM-ABE -whichFST 1 -maxIter 5000
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TM.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/KM-TM.folded.sfs
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst index ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TM.saf.idx -sfs ANGSD_basin-wide/KM-TM.folded.sfs -fold 1 -fstout ANGSD_basin-wide/KM-TM -whichFST 1 -maxIter 5000
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/ABE.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/TC-ABE.folded.sfs
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst index ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/ABE.saf.idx -sfs ANGSD_basin-wide/TC-ABE.folded.sfs -fold 1 -fstout ANGSD_basin-wide/TC-ABE -whichFST 1 -maxIter 5000
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/TM.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/TC-TM.folded.sfs
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst index ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/TM.saf.idx -sfs ANGSD_basin-wide/TC-TM.folded.sfs -fold 1 -fstout ANGSD_basin-wide/TC-TM -whichFST 1 -maxIter 5000
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/ABE.saf.idx ANGSD_basin-wide/TM.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/ABE-TM.folded.sfs
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst index ANGSD_basin-wide/ABE.saf.idx ANGSD_basin-wide/TM.saf.idx -sfs ANGSD_basin-wide/ABE-TM.folded.sfs -fold 1 -fstout ANGSD_basin-wide/ABE-TM -whichFST 1 -maxIter 5000
+realSFS ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TC.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/KM-TC.folded.sfs
+realSFS fst index ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TC.saf.idx -sfs ANGSD_basin-wide/KM-TC.folded.sfs -fold 1 -fstout ANGSD_basin-wide/KM-TC -whichFST 1 -maxIter 5000
+realSFS ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/ABE.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/KM-ABE.folded.sfs
+realSFS fst index ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/ABE.saf.idx -sfs ANGSD_basin-wide/KM-ABE.folded.sfs -fold 1 -fstout ANGSD_basin-wide/KM-ABE -whichFST 1 -maxIter 5000
+realSFS ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TM.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/KM-TM.folded.sfs
+realSFS fst index ANGSD_basin-wide/KM.saf.idx ANGSD_basin-wide/TM.saf.idx -sfs ANGSD_basin-wide/KM-TM.folded.sfs -fold 1 -fstout ANGSD_basin-wide/KM-TM -whichFST 1 -maxIter 5000
+realSFS ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/ABE.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/TC-ABE.folded.sfs
+realSFS fst index ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/ABE.saf.idx -sfs ANGSD_basin-wide/TC-ABE.folded.sfs -fold 1 -fstout ANGSD_basin-wide/TC-ABE -whichFST 1 -maxIter 5000
+realSFS ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/TM.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/TC-TM.folded.sfs
+realSFS fst index ANGSD_basin-wide/TC.saf.idx ANGSD_basin-wide/TM.saf.idx -sfs ANGSD_basin-wide/TC-TM.folded.sfs -fold 1 -fstout ANGSD_basin-wide/TC-TM -whichFST 1 -maxIter 5000
+realSFS ANGSD_basin-wide/ABE.saf.idx ANGSD_basin-wide/TM.saf.idx -tole 1e-6 -maxIter 5000 -fold 1 -P ${SLURM_CPUS_ON_NODE} > ANGSD_basin-wide/ABE-TM.folded.sfs
+realSFS fst index ANGSD_basin-wide/ABE.saf.idx ANGSD_basin-wide/TM.saf.idx -sfs ANGSD_basin-wide/ABE-TM.folded.sfs -fold 1 -fstout ANGSD_basin-wide/ABE-TM -whichFST 1 -maxIter 5000
 
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst stats ANGSD_basin-wide/KM-TC.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/KM-TC.fst.txt
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst stats ANGSD_basin-wide/KM-ABE.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/KM-ABE.fst.txt
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst stats ANGSD_basin-wide/KM-TM.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/KM-TM.fst.txt
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst stats ANGSD_basin-wide/TC-ABE.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/TC-ABE.fst.txt
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst stats ANGSD_basin-wide/TC-TM.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/TC-TM.fst.txt
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS fst stats ANGSD_basin-wide/ABE-TM.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/ABE-TM.fst.txt
+realSFS fst stats ANGSD_basin-wide/KM-TC.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/KM-TC.fst.txt
+realSFS fst stats ANGSD_basin-wide/KM-ABE.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/KM-ABE.fst.txt
+realSFS fst stats ANGSD_basin-wide/KM-TM.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/KM-TM.fst.txt
+realSFS fst stats ANGSD_basin-wide/TC-ABE.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/TC-ABE.fst.txt
+realSFS fst stats ANGSD_basin-wide/TC-TM.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/TC-TM.fst.txt
+realSFS fst stats ANGSD_basin-wide/ABE-TM.fst.idx -whichFST 1 -maxIter 5000 > ANGSD_basin-wide/ABE-TM.fst.txt
 
 for POP in Before After;
 do
 IND=`cat ${POP}.list | wc -l`
 FRAC=`echo "${IND}*0.75" | bc | awk '{print int($1+0.5)}'`
 angsd -P ${SLURM_CPUS_ON_NODE} -bam ${POP}.list -ref A_boucheti.Trinity.merged95.filtered.fasta -anc A_boucheti.Trinity.merged95.filtered.fasta -gl 1 -baq 1 -C 50 -minInd ${FRAC} -minMapQ 30 -minQ 20 -setMinDepth 2 -setMaxDepth 160 -doCounts 1 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -doMaf 1 -doMajorMinor 4 -doSaf 2 -indF ANGSD_basin-wide/${POP}.indF -out ANGSD_basin-wide/${POP}
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS ANGSD_basin-wide/${POP}.saf.idx -tole 1e-6 -maxIter 5000 -P ${SLURM_CPUS_ON_NODE} -fold 1 > ANGSD_basin-wide/${POP}.sfs
-angsd -P ${SLURM_CPUS_ON_NODE} -bam ${POP}.list -ref A_boucheti.Trinity.merged95.filtered.fasta -anc A_boucheti.Trinity.merged95.filtered.fasta -gl 1 -baq 1 -C 50 -minInd ${FRAC} -minMapQ 30 -minQ 20 -setMinDepth 2 -setMaxDepth 160 -doCounts 1 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -doMaf 1 -doMajorMinor 4 -doSaf 2 -indF ANGSD_basin-wide/${POP}.indF -doThetas 1 -pest ANGSD_basin-wide/${POP}.sfs -out ANGSD_basin-wide/${POP}
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/realSFS saf2theta ANGSD_basin-wide/${POP}.saf.idx -outname ANGSD_basin-wide/${POP} -sfs ANGSD_basin-wide/${POP}.sfs -fold 1
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/thetaStat do_stat ANGSD_basin-wide/${POP}.thetas.idx
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/thetaStat print ANGSD_basin-wide/${POP}.thetas.idx > ANGSD_basin-wide/${POP}.thetas
-/gpfs/data/rbeinart/Software/ngsTools/angsd/misc/thetaStat do_stat ANGSD_basin-wide/${POP}.thetas.idx -win 500 -step 100 -outnames ANGSD_basin-wide/${POP}.thetas
+realSFS ANGSD_basin-wide/${POP}.saf.idx -tole 1e-6 -maxIter 5000 -P ${SLURM_CPUS_ON_NODE} -fold 1 > ANGSD_basin-wide/${POP}.sfs
+realSFS saf2theta ANGSD_basin-wide/${POP}.saf.idx -outname ANGSD_basin-wide/${POP} -sfs ANGSD_basin-wide/${POP}.sfs -fold 1
+thetaStat do_stat ANGSD_basin-wide/${POP}.thetas.idx
+thetaStat print ANGSD_basin-wide/${POP}.thetas.idx > ANGSD_basin-wide/${POP}.thetas
+thetaStat do_stat ANGSD_basin-wide/${POP}.thetas.idx -win 500 -step 100 -outnames ANGSD_basin-wide/${POP}.thetas
 perl extract_thetas.pl ANGSD_basin-wide/sites.txt ANGSD_basin-wide/${POP}.thetas ANGSD_basin-wide/${POP}.sites.thetas
 angsd -P ${SLURM_CPUS_ON_NODE} -bam ${POP}.list -ref A_boucheti.Trinity.merged95.filtered.fasta -gl 1 -baq 1 -C 50 -minInd ${FRAC} -minMapQ 30 -minQ 20 -setMinDepth 2 -setMaxDepth 160 -doCounts 1 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -doMaf 1 -doMajorMinor 1 -doHWE 1 -doGlf 1 -indF ANGSD_basin-wide/${POP}.indF -out ANGSD_basin-wide/${POP}
 gunzip ANGSD_basin-wide/${POP}.hwe.gz
